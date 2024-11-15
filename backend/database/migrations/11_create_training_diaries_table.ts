a4b0c0c5b3e1929f4783641a6f26253349453ee4
dydyyyy <ameslantadrien@gmail.com>
2024-11-15 11:17:08 +0100

    add more

---
+++ backend/database/migrations/11_create_training_diaries_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'training_diaries'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('idTrainingDiary').primary()
+      table.json('semesterGrades')
+      table.json('documentList')
+      table.integer('evaluation')
+      table.json('listInterview')
+      table.json('listReport')
+      table.json('listPresentation')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}

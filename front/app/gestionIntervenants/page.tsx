ef11f0972d1e8fe2f820765ba56c280fb23e29ba
dydyyyy <ameslantadrien@gmail.com>
2024-12-18 09:51:56 +0100

    Gestion Intervenants

--- front/app/gestionIntervenants/page.tsx
+++ front/app/gestionIntervenants/page.tsx
@@ -3,38 +3,54 @@ import Home from '@/components/Home'
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import BaseMultiAjout from '@/components/BaseMultiAjout'
+import { postRequest } from '@/api/api';
 
 const GestionIntervenants = () => { 
-  const [rows, setRows] = useState([{ nom: '', prenom: '', email: '' }]);
+ const [rows, setRows] = useState([{ nom: '', prenom: '', email: '', entreprise: '' }]);
 
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
+   const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const role = localStorage.getItem('role');
-    if (role !== 'apprenticeship_coordinators') {
-      router.push('/Login');
-    } else {
-      setIsLoading(false);
-    }
-  }, [router]);
+      const role = localStorage.getItem('role');
+      if (role !== 'apprenticeship_coordinators') {
+        router.push('/Login');
+      } else {
+        setIsLoading(false);
+      }
+    }, [router]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>, rowIndex: number, fieldName: string) => {
-    const newRows = rows.map((row, i) => 
-      i === rowIndex ? { ...row, [fieldName]: e.target.value } : row
-    );
-    setRows(newRows);
-  };
+      const newRows = rows.map((row, i) => 
+        i === rowIndex ? { ...row, [fieldName]: e.target.value } : row
+      );
+      setRows(newRows);
+    };
 
-  const addRow = () => {
-    setRows([...rows, { nom: '', prenom: '', email: '' }]);
-  };
+    const addRow = () => {
+      setRows([...rows, { nom: '', prenom: '', email: '', entreprise: '' }]);
+    };
 
-  //TODO
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(rows);
-  };
+      e.preventDefault();
+      const url = 'professional/createOrUpdateProfessionals';
+      const formattedData = {
+        data: rows.map(row => ({
+          name: row.prenom,
+          lastName: row.nom,
+          email: row.email,
+          companyName: row.entreprise
+        }))
+      };
+      postRequest(url, JSON.stringify(formattedData))
+        .then(response => {
+         console.log('Success:', response);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+      console.log(formattedData);
+    };
 
   if (isLoading) {
     return (  
@@ -47,7 +63,7 @@ const GestionIntervenants = () => {
       </div>
     );
   }
-
+  
   return (
     <Home>
         <div className="flex space-x-4 p-4 w-fit">

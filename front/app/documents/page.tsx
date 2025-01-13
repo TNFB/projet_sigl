1181eaf1eea13b8dfd3898662c7febb23fa35a4e
Corentin-A <avril.corentin@gmail.com>
2025-01-13 21:36:48 +0100

    fix prettier error

--- front/app/documents/page.tsx
+++ front/app/documents/page.tsx
@@ -327,9 +327,7 @@ function Documents() {
                       </button>
                     )}
                     <button
-                      onClick={() =>
-                        handleDownload(doc.document_path)
-                      }
+                      onClick={() => handleDownload(doc.document_path)}
                       className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2'
                     >
                       Télécharger
